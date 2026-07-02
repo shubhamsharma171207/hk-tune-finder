593a1f4285d685c82a2980164e29c02f2a2369df
shubham <shubhamsharma171207@gmail.com>
2026-07-02 13:39:06 +0530

    Update firebase config with environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCO40ZUkDMVqK14ZyhBi6z2mjSzTve_HVg",
-  authDomain: "hk-tune-finder.firebaseapp.com",
-  projectId: "hk-tune-finder",
-  storageBucket: "hk-tune-finder.firebasestorage.app",
-  messagingSenderId: "672915359557",
-  appId: "1:672915359557:web:da1eb274f553b9aaf6a02c",
-  measurementId: "G-PYWYMVQF9S"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+// Initialize Services
+export const db = getFirestore(app);
+export const storage = getStorage(app);
